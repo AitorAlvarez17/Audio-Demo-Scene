a86d9dc4f777580fa67fa8618319edfd5d151b37
Eduardiko <emb2304@yahoo.es>
2022-05-19 23:56:48 +0200

    Change Music Script

---
+++ DemoScene/Assets/ManualMusicChange.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ManualMusicChange : MonoBehaviour
+{
+    public List<AudioMixerSnapshot> tracklist;
+
+    public float transitionTime = 0.2f;
+
+    private int iterator = 0;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if (iterator > tracklist.Count)
+                tracklist[iterator].TransitionTo(transitionTime);
+            else
+                iterator = 0;
+        }
+    }
+}
